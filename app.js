95606e3c0060acd8ad448b39171e45011a7db591
뎁온누리 <devonnuri@gmail.com>
2018-02-14 12:36:09 +0900

    asdasdasd

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
+// const Discord = require('discord.js');
+// const client = new Discord.Client();
+// const fs = require('fs');
+
+// const prefix = "부..부..분배법칙..! ";
+
+// let userdata = JSON.parse(fs.readFileSync('./user.json', 'utf8'));
+
+// function updateData() {
+//     fs.writeFile('./user.json', JSON.stringify(userdata), console.error);
+// }
+
+// client.on('ready', () => {
+//     console.log('I am ready!!');
+// });
+
+// client.on('message', message => {
+//     if(message.content == '!레벨') {
+//         message.channel.send("안녕하세요");
+//         message.author.send(prefix+'당신의 현재 레벨은 '+userdata[message.author.id]["level"]+'입니다!');
+//     } else {
+//         if(userdata[message.author.id] != undefined) {
+//             userdata[message.author.id]["level"] = 0;
+//         }
+//         userdata[message.author.id]["level"] += 1;
+//         updateData();
+//     }
+// });
+
+// client.on('guildMemberAdd', message => {
+//     member.send(prefix+"공주중배그 서버에 오신 것을 환영합니다..!");
+//     userdata[message.author.id]["level"] = 0;
+//     updateData();
+// });
+
+// client.login(process.env.BOT_TOKEN);
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require('fs');
-
-const prefix = "부..부..분배법칙..! ";
-
-let userdata = JSON.parse(fs.readFileSync('./user.json', 'utf8'));
-
-function updateData() {
-    fs.writeFile('./user.json', JSON.stringify(userdata), console.error);
-}
 
 client.on('ready', () => {
     console.log('I am ready!!');
 });
 
 client.on('message', message => {
-    if(message.content == '!레벨') {
-        message.channel.send("안녕하세요");
-        message.author.send(prefix+'당신의 현재 레벨은 '+userdata[message.author.id]["level"]+'입니다!');
-    } else {
-        if(userdata[message.author.id] != undefined) {
-            userdata[message.author.id]["level"] = 0;
-        }
-        userdata[message.author.id]["level"] += 1;
-        updateData();
+    if(message.content == "동연아") {
+        message.reply("ㅁㄴㅇㄹ");
     }
-});
-
-client.on('guildMemberAdd', message => {
-    member.send(prefix+"공주중배그 서버에 오신 것을 환영합니다..!");
-    userdata[message.author.id]["level"] = 0;
-    updateData();
 });
 
 client.login(process.env.BOT_TOKEN);
