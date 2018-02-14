77bfcbf74e36946b5d6b0073b5b823c67619ea79
뎁온누리 <devonnuri@gmail.com>
2018-02-14 12:42:50 +0900

    ㅁㄴㅇㄻㄴㅇㄹ

--- app.js
+++ app.js
@@ -4,28 +4,33 @@ const fs = require('fs');
 
 const prefix = "부..부..분배법칙..! ";
 
-let userdata = JSON.parse(fs.readFileSync('./user.json', 'utf8'));
+// let userdata = JSON.parse(fs.readFileSync('./user.json', 'utf8'));
 
-function updateData() {
-    fs.writeFile('./user.json', JSON.stringify(userdata), console.error);
-}
+// function updateData() {
+//     fs.writeFile('./user.json', JSON.stringify(userdata), console.error);
+// }
 
-client.on('ready', () => {
-    console.log('I am ready!!');
-});
+// client.on('ready', () => {
+//     console.log('I am ready!!');
+// });
+
+// client.on('message', message => {
+//     if(message.content == '!레벨') {
+//         message.reply(prefix+'당신의 현재 레벨은 '+userdata[message.author.id]["level"]+'입니다!');
+//     } else {
+//         if(userdata[message.author.id] != undefined) {
+//             userdata[message.author.id]["level"] = 0;
+//         }
+//         userdata[message.author.id]["level"] += 1;
+//         updateData();
+//     }
+// });
 
 client.on('message', message => {
     if(message.content == '!레벨') {
-        message.channel.send("안녕하세요");
-        message.author.send(prefix+'당신의 현재 레벨은 '+userdata[message.author.id]["level"]+'입니다!');
-    } else {
-        if(userdata[message.author.id] != undefined) {
-            userdata[message.author.id]["level"] = 0;
-        }
-        userdata[message.author.id]["level"] += 1;
-        updateData();
+        message.reply("ㅁㄴㅇㄹ");
     }
-});
+})
 
 // client.on('guildMemberAdd', message => {
 //     member.send(prefix+"공주중배그 서버에 오신 것을 환영합니다..!");
