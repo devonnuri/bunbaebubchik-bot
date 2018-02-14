eb74b0b1332df56d00319e3a5fda42444ea3eb8d
뎁온누리 <devonnuri@gmail.com>
2018-02-14 11:19:43 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!!');
+});
+
+client.on('message', message => {
+    if(message.content == "동연아") {
+        message.reply("ㅁㄴㅇㄹ");
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
