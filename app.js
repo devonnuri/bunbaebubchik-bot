4ca7451da691fbd87c98361953b317bbec4dd3a1
뎁온누리 <devonnuri@gmail.com>
2018-02-14 12:33:57 +0900

    asdf

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const fs = require('fs')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
 
-const prefix = "부..부..분배법칙..! "
+const prefix = "부..부..분배법칙..! ";
 
-let userdata = JSON.parse(fs.readFileSync('./user.json', 'utf8'))
+let userdata = JSON.parse(fs.readFileSync('./user.json', 'utf8'));
 
 function updateData() {
-    fs.writeFile('./user.json', JSON.stringify(userdata), console.error)
+    fs.writeFile('./user.json', JSON.stringify(userdata), console.error);
 }
 
 client.on('ready', () => {
-    console.log('I am ready!!')
+    console.log('I am ready!!');
 });
 
 client.on('message', message => {
     if(message.content == '!레벨') {
         message.channel.send("안녕하세요");
-        message.author.send(prefix+'당신의 현재 레벨은 '+userdata[message.author.id]["level"]+'입니다!')
+        message.author.send(prefix+'당신의 현재 레벨은 '+userdata[message.author.id]["level"]+'입니다!');
     } else {
         if(userdata[message.author.id] != undefined) {
             userdata[message.author.id]["level"] = 0;
@@ -28,9 +28,9 @@ client.on('message', message => {
 });
 
 client.on('guildMemberAdd', message => {
-    member.send(prefix+"공주중배그 서버에 오신 것을 환영합니다..!")
+    member.send(prefix+"공주중배그 서버에 오신 것을 환영합니다..!");
     userdata[message.author.id]["level"] = 0;
     updateData();
 });
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
